4091bfeeb1e30d5d5c5aee3f284baf025dfc3ef4
abhijeet <chougaleabhii@gmail.com>
2024-04-16 03:50:42 +0530

    adding bootstrap path in index.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Navbar } from 'react-bootstrap';
-
+import Navbar from './Component/Navbar';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Employee />} />
+          {/* <Route path="/" element={<Employee />} />
           <Route path="/Attendence" element={<Attendence />} />
           <Route path="/Advance" element={<Advance />} />
           <Route path="/Leave" element={<Leave />} />
-          <Route path="/Salary" element={<Salary />} />
+          <Route path="/Salary" element={<Salary />} /> */}
 
         </Routes>
       </BrowserRouter>
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -5,27 +5,27 @@ const Navbar = () => {
     return (
 
         <>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <Link class="navbar-brand" href="#">Hidden brand</Link>
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <Link class="nav-link " to="" >Home</Link>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        <Link className="navbar-brand" href="#">Nvabar brand</Link>
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link " to="" >Home</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" to="">Link</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="">Link</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link " to="" ></Link>
+                            <li className="nav-item">
+                                <Link className="nav-link " to="" ></Link>
                             </li>
                         </ul>
-                        <form class="d-flex">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
                         </form>
                     </div>
                 </div>
