43d1125e868bc1f519c09bc8d7dc6f745dfe1e04
abhijeet <chougaleabhii@gmail.com>
2024-04-18 17:43:55 +0530

    navbar changes

--- src/App.js
+++ src/App.js
@@ -6,22 +6,24 @@ import ProjectUser from './Pages/ProjectUser';
 import TicketStatus from './Pages/TicketStatus';
 import TicketType from './Pages/TicketType';
 import CreateProject from './Pages/CreateProject';
-import { Navbar } from 'react-bootstrap';
+import Navbar from './Pages/Navbar';
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-       <BrowserRouter>
-            <Routes>
-              <Route path="user" element={<User></User>}></Route>
-              <Route path="/project" element={<CreateProject/>}></Route>
-              <Route path="/issues" element={<Issues></Issues>}></Route>
-              <Route path="/projectUser" element={<ProjectUser></ProjectUser>}></Route>            
-              <Route path="/ticketType" element={<TicketType></TicketType>}></Route>
-              <Route path="/ticketStatus" element={<TicketStatus></TicketStatus>}></Route>               
-            </Routes>
-          </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+
+        <Routes>
+          <Route path="user" element={<User></User>}></Route>
+          <Route path="/project" element={<CreateProject />}></Route>
+          <Route path="/issues" element={<Issues></Issues>}></Route>
+          <Route path="/projectUser" element={<ProjectUser></ProjectUser>}></Route>
+          <Route path="/ticketType" element={<TicketType></TicketType>}></Route>
+          <Route path="/ticketStatus" element={<TicketStatus></TicketStatus>}></Route>
+        </Routes>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -3,43 +3,43 @@ import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
-    return (
+  return (
 
-        <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-              <div className="container-fluid">
-                <a className="navbar-brand" href="#">GlitchFound</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  
-                  <li className="nav-item">
-                    <Link className='nav-link' to="/user">User</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className='nav-link' to="/project">Project</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className='nav-link' to="/issues">Issues</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className='nav-link' to="/projectUser">ProjectUser</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className='nav-link' to="/ticketType">TicketType</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className='nav-link' to="/ticketStatus">TicketStatus</Link>
-                  </li>  
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+        <div className="container-fluid">
+          <Link className="navbar-brand">GlitchFound</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
-                </ul>
-                </div>
-              </div>
-            </nav>
-        </>
-    );
+              <li className="nav-item">
+                <Link className='nav-link' to="/user">User</Link>
+              </li>
+              <li className="nav-item">
+                <Link className='nav-link' to="/project">Project</Link>
+              </li>
+              <li className="nav-item">
+                <Link className='nav-link' to="/issues">Issues</Link>
+              </li>
+              <li className="nav-item">
+                <Link className='nav-link' to="/projectUser">ProjectUser</Link>
+              </li>
+              <li className="nav-item">
+                <Link className='nav-link' to="/ticketType">TicketType</Link>
+              </li>
+              <li className="nav-item">
+                <Link className='nav-link' to="/ticketStatus">TicketStatus</Link>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
